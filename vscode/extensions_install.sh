b4b70145c4ee66224643afcfeb822b4d2914a1cf
nikolay <nugusev@gmail.com>
2022-12-29 10:44:28 +0100

    extensions_install on mac - wip

---
+++ vscode/extensions_install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+code --install-extension alefragnani.numbered-bookmarks
+code --install-extension asvetliakov.vscode-neovim
+code --install-extension esbenp.prettier-vscode
+code --install-extension geeebe.duplicate
+code --install-extension kahole.magit
+code --install-extension mhutchie.git-graph
+code --install-extension PKief.material-icon-theme
+code --install-extension tombonnike.vscode-status-bar-format-toggle
+code --install-extension VSpaceCode.vspacecode
+code --install-extension VSpaceCode.whichkey

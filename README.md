d523372152ac1dc34dc6bb096a1f374957e1826f
nikolay <nugusev@gmail.com>
2022-12-29 10:33:45 +0100

    Symlink script for unix. Uninstall nvim customizations script for unix.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Windows
+* Install nvim from .msi
+* Install vim-plug (see github repo for instructions)
+* run windows_symlink.ps1
+* run extensions_install.ps1
--- unix_symlink.sh
+++ unix_symlink.sh
@@ -1,1 +1,12 @@
-todo
+#! /bin/bash
+
+ln -fns $(pwd)/nvim ~/.config/
+
+for filename in vscode/*.*; do
+    name=${filename##*/}
+    base=${name%}
+    # echo $filename
+    # echo $(pwd)/${filename}
+    # echo ~/Library/"Application Support"/Code/User/$base
+    ln -fns $(pwd)/${filename} ~/Library/"Application Support"/Code/User/$base
+done

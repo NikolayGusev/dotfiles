402ff28c5e963253cfb2d2136ea2a7014c05bee2
nikolay <nugusev@gmail.com>
2023-01-25 21:27:13 +0100

    Better test example for refactorings.

---
+++ z_test/sumStrings.ts
@@ -0,0 +1,4 @@
+
+export function sumStrings(d: string, hello: string, test: string) {
+    return d + hello;
+}
--- z_test/test2.ts
+++ z_test/test2.ts
@@ -1,13 +1,23 @@
-const a:number = 123;
+import { sumStrings } from './sumStrings';
 
+const a:number = 123456;
+
+interface A {
+    a:  string,
+    b: number,
+    c: string,
+}
+
+const f2 = () => console.log('f2'); 
 
 function f1(a: string, b: string, extracted: {d: string, e?: string}) {
+    if (1 === 2-1) {
+       console.log(123) ;
+    }
     const hello = 'hello';
     const { d } = extracted;
-    const f = sumStrings(d, hello);
-    return a + sumStrings(b,d) + hello + hello;
+    const f = sumStrings(d, hello, 'asdf');
+    return a + sumStrings(b,d, 'fdsa') + hello + hello;
 }
 
-function sumStrings(d: string, hello: string) {
-    return d + hello;
-}
+

8c9581e39b932d5ec1730239cb8418ff33a473ea
nikolay <nugusev@gmail.com>
2023-01-25 21:06:15 +0100

    Better test2.ts example.

--- z_test/test2.ts
+++ z_test/test2.ts
@@ -1,5 +1,13 @@
-const a:string = 123;
+const a:number = 123;
 
-function f1(a: string, b: string) {
-    return a + b + 'hello';
+
+function f1(a: string, b: string, extracted: {d: string, e?: string}) {
+    const hello = 'hello';
+    const { d } = extracted;
+    const f = sumStrings(d, hello);
+    return a + sumStrings(b,d) + hello + hello;
+}
+
+function sumStrings(d: string, hello: string) {
+    return d + hello;
 }

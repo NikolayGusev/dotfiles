1cf72afe7a6df8e22e7f5e4887aaf6246a8e2b5f
nikolay <nugusev@gmail.com>
2023-04-29 11:06:37 +0200

    Better lazygit config. Lazygit symlink in script.

--- unix_symlink.sh
+++ unix_symlink.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 ln -fns $(pwd)/nvim ~/.config/
+ln -fns $(pwd)/git/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 
 for filename in vscode/*.*; do
     name=${filename##*/}

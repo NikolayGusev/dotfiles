be622e228504ffbda530b349492d7b748b6242dd
nikolay <nugusev@gmail.com>
2023-08-15 20:37:47 +0200

    minor

--- unix_symlink.sh
+++ unix_symlink.sh
@@ -5,6 +5,7 @@ ln -fns $(pwd)/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json
 ln -fns $(pwd)/git/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 ln -fns $(pwd)/terminal/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -fns $(pwd)/idea/.ideavimrc ~/.ideavimrc
+ln -fns $(pwd)/mac/.amethyst.yml ~/.amethyst.yml
 
 for filename in vscode/*.*; do
     name=${filename##*/}

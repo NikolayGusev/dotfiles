b3a18408c4d3a664d80d28761d7e0b44ec93a39e
Nikolay Gusev <nikolaygusev@Nikolays-MacBook-Pro.local>
2023-12-15 11:21:52 +0100

    open telescope "Find Files" by default when opening nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -896,6 +896,14 @@ vim.cmd([[
   augroup END
 ]])
 
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argv(0) == "" then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
+
 
 function format_with_undojoin()
   local undotree = vim.fn.undotree()
